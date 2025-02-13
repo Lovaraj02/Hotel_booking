c21d11d4c5345532a90044c112fddc6607d22733
lovaraj02 <rajubandaru2255@gmail.com>
2025-02-13 16:41:43 +0530

    Initial commit

---
+++ front end/src/linkingpage.js
@@ -0,0 +1,17 @@
+// import { Link } from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+
+// function Linkingpage() {
+//   return (
+//     <div className="container-fluid" style={{ backgroundImage: "url('link.jpg')", height: '900px', backgroundSize: 'cover' }}>
+//       <Link to="/menu">
+//         <button className="btn  mt-3 bg-none" style={{color:"white"}}><h3>Menu</h3></button>
+//       </Link>
+//       <Link to="/booking">
+//         <button className="btn  mt-3 bg-none" style={{color:"white"}}><h3>Booking</h3></button>
+//       </Link>
+//     </div>
+//   );
+// }
+
+// export default Linkingpage;
